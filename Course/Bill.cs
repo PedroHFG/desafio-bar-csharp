92c7e516d466879f9227a3f713ebf1b5a5c9e3f3
PedroHFG <pedrohfidg@gmail.com>
2024-12-26 09:59:54 -0300

    Final project

---
+++ Course/Bill.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+
+
+namespace Course
+{
+    internal class Bill
+    {
+        public char Gender;
+        public int Beer;
+        public int Barbecue;
+        public int SoftDrink;
+
+        public double Cover()
+        {
+            if (Feeding() < 30.0)
+            {
+                return 4.0;
+            }
+            else 
+            {
+                return 0.0;
+            }
+        }
+
+        public double Feeding()
+        {
+            return Beer * 5.0 + SoftDrink * 3.0 + Barbecue * 7.0;
+
+        }
+
+        public double Ticket()
+        {
+            double ticket = 0.0;
+            if (Gender == 'M')
+            {
+                ticket = 10.0;
+            }
+            else
+            {
+                ticket = 8.0;
+            }
+            return ticket;
+        }
+
+        public double Total()
+        {
+            return Ticket() + Feeding() + Cover();
+
+        }
+
+        public override string ToString()
+        {
+            return "RELATÓRIO:" 
+                + "\nConsumo = R$ " + Feeding().ToString("F2", CultureInfo.InvariantCulture)
+                + (Cover() > 0.0 ? "\nCouvert = R$ " + Cover().ToString("F2", CultureInfo.InvariantCulture) : "\nIsento de Couvert")
+                + "\nIngresso = R$ " + Ticket().ToString("F2", CultureInfo.InvariantCulture)
+                + "\n\nValor a pagar = R$ " + Total().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Bill bill = new Bill();
+
+            Console.Write("Sexo: ");
+            bill.Gender = char.Parse(Console.ReadLine().ToUpper());
+            while (bill.Gender != 'F' && bill.Gender != 'M')
+            {
+                Console.Write("Sexo inválido! Por favor, informe o sexo (F ou M): ");
+                bill.Gender = char.Parse(Console.ReadLine().ToUpper());
+            }
+
+            Console.Write("Quantidade de cerveja: ");
+            bill.Beer = int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de refrigerante: ");
+            bill.SoftDrink = int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de espetinho: ");
+            bill.Barbecue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine(bill);
         }
     }
 }
